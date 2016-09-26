43da0016de596327f9ca2fb986396ff929d7a3e1
Aziz <asnobi@gmail.com>
2016-09-26 19:14:19 +0000

    test new

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,33 @@
+class UsersController < ApplicationController
+  def new
+  end
+#  def user_params
+#    params.require(:user).permit(:user, :id, :email, :created_at, :updated_at)
+#  end  
+
+  
+  def login
+    
+  end
+  
+  def create
+    @user = User.new(user_params)
+    
+    if (User.exists?(user_id: @user.user_id)) || (@user.user_id == "")
+      redirect_to new_user_path
+      flash[:notice] = "This User ID is already exists!!"
+    else
+      @user.save
+      
+      redirect_to movies_path
+      flash[:notice] = "Welcome #{@user.user_id}. Your account has been created"
+    end
+
+  end  
+  
+  private
+  def user_params
+    params.require(:user).permit(:user_id, :email, :session_token)
+  end
+  
+end
---
+++ app/models/user.rb
@@ -0,0 +1,14 @@
+class User < ActiveRecord::Base
+    #attr_accessor :user_id, :email, :session_token
+    #@a = User.new(:user_id, :email, :session_token)
+    #@a.session_token = "xXx"
+    #@a.user_id = "test"
+    #@a.save
+
+        
+    
+    
+    
+end
+
+
